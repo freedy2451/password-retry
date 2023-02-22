cba4918938d84ec2ef7e1d53020b35c18c46ac14
Lucy <b237237@gmail.com>
2023-02-22 21:01:19 +0800

    password_v0.02

--- password.py
+++ password.py
@@ -1,13 +1,15 @@
 p_word = 'a123456'
 
-i = 3
-while True:
+i = 3 #剩餘機會
+while i > 0:
+    i = i - 1
     a = input ("請輸入正確的密碼: ")
-    if a != p_word:
-        i = i - 1
-        print("密碼錯誤，還有{}次機會ˇ".format(i))       
-        if i == 0:
-            #print("密碼錯誤3次")
-            break
-    else:
-        print("登入成功")        
+    if a == p_word: 
+        print("登入成功")  
+    else:          
+        print("密碼錯誤")               
+        if i != 0 :
+            print("還有{}次機會ˇ".format(i))
+        else:
+            print("已超過次數，鎖定帳號")
+                
